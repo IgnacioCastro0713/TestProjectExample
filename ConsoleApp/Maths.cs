c4ea06d0f44ca011bfeb991e4c88454941ede867
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2021-08-23 19:45:23 -0500

    Initial commit

---
+++ ConsoleApp/Maths.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp
+{
+    public static class Maths
+    {
+        public static int Sum(int a, int b) => a + b;
+        public static int Subtract(int a, int b) => a - b;
+        public static int Multiplicity(int a, int b) => a * b;
+    }
+}
---
+++ TestProjectNUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TestProjectNUnit
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
